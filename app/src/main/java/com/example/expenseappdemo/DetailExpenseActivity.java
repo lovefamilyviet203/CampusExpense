296f27f32bb17372df459f7d7edf1fc2141a6a80
Admin <sonnguyenbao119@gmail.com>
2023-12-11 04:03:08 +0700

    view update

--- app/src/main/java/com/example/expenseappdemo/DetailExpenseActivity.java
+++ app/src/main/java/com/example/expenseappdemo/DetailExpenseActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import database.DatabaseHelper;
 import database.ExpenseEntity;
@@ -17,9 +18,11 @@ public class DetailExpenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_expense);
+
         ImageButton backButtonForm = findViewById(R.id.imageButtonBackForm);
         ImageButton detailToUpdateButtonForm = findViewById(R.id.imageButtonDetailToUpdate);
         ImageButton detailToHomeButton = findViewById(R.id.imageButtonDetailToHome);
+
         int expenseId = getIntent().getIntExtra("expenseId", -1);
 
         if (expenseId != -1) {
@@ -33,46 +36,56 @@ public class DetailExpenseActivity extends AppCompatActivity {
             TextView expenseDateTextView = findViewById(R.id.expenseDateTextView2);
             TextView expenseCategoryTextView = findViewById(R.id.expenseCategoryTextView2);
 
-            // Add more TextViews or other UI elements as needed
-
             // Populate the UI with expense details
-            expenseNameTextView.setText("Expense Name: " + expense.expenseName);
-            expenseAmountTextView.setText("Amount: " + expense.amount);
-            expenseDateTextView.setText("Date : " + expense.expenseDate);
-            expenseCategoryTextView.setText("Category : " + expense.expenseType);
+            expenseNameTextView.setText("Expense Name: " + expense.getExpenseName());
+            expenseAmountTextView.setText("Amount: " + expense.getAmount());
+            expenseDateTextView.setText("Date : " + expense.getExpenseDate());
+            expenseCategoryTextView.setText("Category : " + expense.getExpenseType());
             // Update other UI elements
-        }
-        backButtonForm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                onBackListExpense();
-            }
-        });
-        detailToUpdateButtonForm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDetailtoUpdate();
-            }
-        });
-        detailToHomeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDetailtoHomeButtonClick();
-            }
-        });
 
+            backButtonForm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackListExpense();
+                }
+            });
+
+            detailToUpdateButtonForm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onDetailtoUpdate(expense);
+                }
+            });
+
+            detailToHomeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onDetailtoHomeButtonClick();
+                }
+            });
+        } else {
+            // Handle the case where expenseId is -1 (invalid)
+            Toast.makeText(this, "Invalid Expense ID", Toast.LENGTH_SHORT).show();
+            finish(); // Finish the activity if the expense ID is invalid
+        }
     }
-    public void onBackListExpense(){
+
+    public void onBackListExpense() {
         Intent intent = new Intent(this, ListExpenseActivity.class);
         startActivity(intent);
         finish();
     }
-    public void onDetailtoUpdate(){
+
+    public void onDetailtoUpdate(ExpenseEntity selectedExpense) {
         Intent intent = new Intent(this, UpdateExpenseActivity.class);
+        intent.putExtra("expenseId", selectedExpense.getId());
+        intent.putExtra("expenseAmount", selectedExpense.getAmount());
+        intent.putExtra("expenseDate", selectedExpense.getExpenseDate()); // Assuming date is a long
         startActivity(intent);
         finish();
     }
-    public void onDetailtoHomeButtonClick(){
+
+    public void onDetailtoHomeButtonClick() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
